dc33f4d530a5563c85fbb1d7059600351337dd5c
jugal-ionic <jugal.ionic@gmail.com>
2016-08-18 21:52:03 +0530

    as per current live as on 18/08/2016
    Signed-off-by: jugal-ionic <jugal.ionic@gmail.com>

--- deploy/public/assets/js/main.js
+++ deploy/public/assets/js/main.js
@@ -6252,15 +6252,37 @@ Description:
 
 *********************/
 function HomePageScreen() {
-    var self = this, startDate = new Date(2016, 7, 18, 12, 0, 0, 0), now = new Date(), voucherCTA, checkDate;
+    var self = this, startDate = new Date(2016, 7, 18, 12, 0, 0, 0), now = new Date(), voucherCTA, errorMessage = document.getElementById("dateErrorMessage"), errorWrapper = document.getElementById("error-overlay"), checkDate;
     Screen.apply(this, Array.prototype.slice.call(arguments));
     this.id = "home-page-screen";
     this.name = "Home page";
     this.templateId = "home-page-template";
     function loadMapPage(e) {
         e.preventDefault();
-        self.scrManager.addScreen(MapPageScreen);
-        ga("send", "event", "Find a bar", "click", "click initial button");
+        userCoordinates = JSON.parse(localStorage.getItem("usersLocation"));
+        if (userCoordinates[0] === 52.6344122 && userCoordinates[1] === -1.1368473) {
+            // Opera 8.0+
+            var isOpera = !!window.opr && !!opr.addons || !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0;
+            // Firefox 1.0+
+            var isFirefox = typeof InstallTrigger !== "undefined";
+            // At least Safari 3+: "[object HTMLElementConstructor]"
+            var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf("Constructor") > 0;
+            // Internet Explorer 6-11
+            var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+            // Edge 20+
+            var isEdge = !isIE && !!window.StyleMedia;
+            // Chrome 1+
+            var isChrome = !!window.chrome && !!window.chrome.webstore;
+            // Blink engine detection
+            var isBlink = (isChrome || isOpera) && !!window.CSS;
+            if (isFirefox) {
+                errorMessage.innerHTML = "PLEASE REFRESH BROWSER AND ENABLE LOCATION SERVICE TO VIEW BARS";
+                errorWrapper.style.display = "block";
+            }
+        } else {
+            self.scrManager.addScreen(MapPageScreen);
+            ga("send", "event", "Find a bar", "click", "click initial button");
+        }
     }
     function loadEnterVoucherPage(e) {
         e.preventDefault();
@@ -7647,8 +7669,24 @@ function errorPositionCallBack() {
     //alert("Please enable geolocation on your device");
     localStorage.setItem("usersLocation", JSON.stringify([ 52.6344122, -1.1368473 ]));
     var errorMessage = document.getElementById("dateErrorMessage"), errorWrapper = document.getElementById("error-overlay");
-    errorMessage.innerHTML = "PLEASE REFRESH BROWSER AND ENABLE LOCATION SERVICE TO VIEW BARS";
-    errorWrapper.style.display = "block";
+    // Opera 8.0+
+    var isOpera = !!window.opr && !!opr.addons || !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0;
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== "undefined";
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf("Constructor") > 0;
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1+
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+    // Blink engine detection
+    var isBlink = (isChrome || isOpera) && !!window.CSS;
+    if (isFirefox) {
+        errorMessage.innerHTML = "PLEASE REFRESH BROWSER AND ENABLE LOCATION SERVICE TO VIEW BARS";
+        errorWrapper.style.display = "block";
+    }
 }
 
 // Listen for orientation changes
--- src/js/App.js
+++ src/js/App.js
@@ -278,8 +278,26 @@ function signUpNewsletter(skipDob) {
 		localStorage.setItem("usersLocation",JSON.stringify([52.6344122,-1.1368473]));
 		var errorMessage = document.getElementById('dateErrorMessage'),
 		errorWrapper = document.getElementById('error-overlay');
-		errorMessage.innerHTML = 'PLEASE REFRESH BROWSER AND ENABLE LOCATION SERVICE TO VIEW BARS';
-		errorWrapper.style.display = 'block';
+		    // Opera 8.0+
+		var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+		    // Firefox 1.0+
+		var isFirefox = typeof InstallTrigger !== 'undefined';
+		    // At least Safari 3+: "[object HTMLElementConstructor]"
+		var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+		    // Internet Explorer 6-11
+		var isIE = /*@cc_on!@*/false || !!document.documentMode;
+		    // Edge 20+
+		var isEdge = !isIE && !!window.StyleMedia;
+		    // Chrome 1+
+		var isChrome = !!window.chrome && !!window.chrome.webstore;
+		    // Blink engine detection
+		var isBlink = (isChrome || isOpera) && !!window.CSS;
+			if(isFirefox)
+			{
+				errorMessage.innerHTML = 'PLEASE REFRESH BROWSER AND ENABLE LOCATION SERVICE TO VIEW BARS';
+				errorWrapper.style.display = 'block';
+			}
+		
 	}
 
 // Listen for orientation changes
